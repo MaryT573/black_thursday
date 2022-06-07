0458d0c9b27a893361e647f566ea10c28a2ef66b
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-06-07 17:42:17 -0600

    fix for merchant

--- test/merchant_spec.rb
+++ test/merchant_spec.rb
@@ -1,7 +1,7 @@
 require './lib/merchant'
 
 RSpec.describe Merchant do
-  let(:merchant) {Merchant.new({:id => 5, :name => "Turing School"})}
+  let(:merchant) {Merchant.new({:id => 5, :name => "Turing School", :created_at => "2012-11-23"})}
 
   it "exists" do
     expect(merchant).to be_an(Merchant)
@@ -10,6 +10,7 @@ RSpec.describe Merchant do
   it "has attributes" do
     expect(merchant.name).to eq("Turing School")
     expect(merchant.id).to eq(5)
+    expect(merchant.created_at).to eq(Time.parse("2012-11-23"))
   end
 
 end
