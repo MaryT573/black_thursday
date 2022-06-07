1ec894b27e6a5fb189b19097b9bb1638c5328efe
Nikky Rojas <rojas7313@gmail.com>
2022-06-07 08:26:50 -0600

    add test for merchant with 1 item and 1 item per month

--- test/sales_analyst_spec.rb
+++ test/sales_analyst_spec.rb
@@ -107,4 +107,13 @@ RSpec.describe SalesAnalyst do
     expect(sales_analyst.invoice_total(1)).to eq(21067.77)
     expect(sales_analyst.invoice_total(1).class).to eq BigDecimal
   end
+  it "which merchant offers only one item" do
+    # binding.pry
+    expect(sales_analyst.merchants_with_only_one_item).to eq([])
+
+  end
+  it "merchants that only sell one item by the month they registered" do
+    expect(sales_analyst.merchants_with_only_one_item_registered_in_month()).to eq([])
+
+  end
 end
